f8eb5bceeb9715926c008e436102290add508b68
wenjian <jian.wen@ubtrobot.com>
2018-09-21 19:55:25 +0800

    代码小优化

--- app/src/main/java/com/wenjian/wanandroid/ui/adapter/ArticleListAdapter.kt
+++ app/src/main/java/com/wenjian/wanandroid/ui/adapter/ArticleListAdapter.kt
@@ -3,7 +3,6 @@ package com.wenjian.wanandroid.ui.adapter
 import android.text.Html
 import android.view.View
 import android.widget.ImageView
-import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.wenjian.wanandroid.R
 import com.wenjian.wanandroid.base.BaseRecyclerAdapter
@@ -21,24 +20,28 @@ class ArticleListAdapter : BaseRecyclerAdapter<Article>(R.layout.rv_item_article
     override fun convert(helper: BaseViewHolder?, item: Article?) {
         helper?.apply {
 
-            item?.let { it ->
-                if (it.chapterName.isNullOrBlank()) {
-                    setText(R.id.tv_category, "")
-                } else {
-                    setText(R.id.tv_category, "${it.chapterName?.trim()}/${it.superChapterName?.trim()}")
+            with(item!!) {
+                val buildString = buildString {
+                    if (!chapterName.isNullOrBlank()) {
+                        append(chapterName?.trim())
+                    }
+                    if (!superChapterName.isNullOrBlank()) {
+                        append("/${superChapterName?.trim()}")
+                    }
                 }
-                setText(R.id.tv_title, Html.fromHtml(it.title?.trim()))
-                setText(R.id.tv_name, it.author?.trim())
-                setText(R.id.tv_date, it.niceDate?.trim())
+                setText(R.id.tv_category, buildString)
+                setText(R.id.tv_title, Html.fromHtml(title?.trim()))
+                setText(R.id.tv_name, author?.trim())
+                setText(R.id.tv_date, niceDate?.trim())
                 val imageView = getView<ImageView>(R.id.iv_image)
-                if (!it.envelopePic.isNullOrBlank()) {
-                    imageView.loadUrl(it.envelopePic)
+                if (!envelopePic.isNullOrBlank()) {
+                    imageView.loadUrl(envelopePic)
                     imageView.visibility = View.VISIBLE
                 } else {
                     imageView.visibility = View.GONE
                 }
                 itemView.setOnClickListener {
-                    WebActivity.start(it.context, item.buildWebModel())
+                    WebActivity.start(it.context, buildWebModel())
                 }
             }
         }
